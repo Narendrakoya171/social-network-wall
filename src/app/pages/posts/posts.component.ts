578ff38d158ee71cced53b5263dd87273661188a
Narendrakoya171 <koya.narendra01@gmail.com>
2022-11-27 01:37:00 +0530

    finall commit

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,6 +1,11 @@
-import { Router } from '@angular/router';
-import { UsersService } from './../../service/users.service';
-import { Component, OnInit } from '@angular/core';
+import {AngularFireStorage} from '@angular/fire/compat/storage';
+
+import {Router} from '@angular/router';
+import {UsersService} from './../../service/users.service';
+import {Component, OnInit} from '@angular/core';
+import {finalize} from 'rxjs';
+import {PostsService} from 'src/app/service/posts.service';
+import { formatPercent } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
@@ -8,7 +13,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
-  constructor(private UsersService: UsersService, private router: Router) {}
+  constructor(
+    public UsersService: UsersService,
+    private router: Router,
+    private storage: AngularFireStorage,
+    private ps: PostsService
+  ) {}
   ngOnInit(): void {
     if (this.UsersService.user == undefined || this.UsersService.user == null) {
       var str = localStorage.getItem('user');
@@ -19,12 +29,153 @@ export class PostsComponent implements OnInit {
         this.router.navigate(['/login']);
       }
     }
+    this.ps
+      .getPosts()
+      .then((res: any) => {
+        console.log(res);
+        this.posts = res;
+
+        for (let post of this.posts) {
+          post.comment.push('');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
+
+  selectedfile: any;
+  text: string = '';
+  posts: Array<any> = [];
+  commentText: Array<any> = [];
+  onPreselected(event: any) {
+    this.selectedfile = event.target.files[0];
+  }
+  post() {
+    if (this.selectedfile != undefined || this.selectedfile != null) {
+      this.uploadImage()
+        .then((imageurl) => {
+          console.log(imageurl);
+
+          let postobj = {
+            userName: this.UsersService.user.username,
+            text: this.text,
+            imageURL: imageurl,
+            likes: [],
+            comments: [],
+          };
+          this.posts.push(postobj);
+          this.ps
+            .postusers(postobj)
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+          this.selectedfile = undefined;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      let objpost = {
+        userName: this.UsersService.user.userName,
+        text: this.text,
+        imageUrl: '',
+        like: [],
+        comments: [],
+      };
+      this.posts.push(objpost);
+      this.ps
+        .postusers(objpost)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+  uploadImage() {
+    return new Promise((reslove, reject) => {
+      let n = Date.now();
+      const file = this.selectedfile;
+      const filepath = `images/${n}`;
+      const fileRef = this.storage.ref(filepath);
+      const task = this.storage.upload(`images/${n}`, file);
+      task
+        .snapshotChanges()
+        .pipe(
+          finalize(() => {
+            let imageURL = fileRef.getDownloadURL();
+            imageURL.subscribe((url: any) => {
+              if (url) {
+                console.log(url);
+                reslove(url);
+              }
+            });
+          })
+        )
+        .subscribe((url) => {
+          if (url) {
+            console.log(url);
+          }
+        });
+    });
+  }
+
+  like(postid: number) {
+    for (let i = 0; i < this.posts.length; i++) {
+      if (this.posts[i].id == postid) {
+        if (this.posts[i].likes.indexOf(this.UsersService.user.id) >= 0) {
+          this.posts[i].likes.splice(
+            this.posts[i].likes.indexOf(this.UsersService.user.id),
+            1
+          );
+        } else {
+          this.posts[i].likes.push(this.UsersService.user.id);
+        }
+        this.ps
+          .like(this.posts[i])
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    }
+  }
+  comment(postId: any, postindex: any) {
+     for (let i = 0; i < this.posts.length; i++) {
+       if (this.posts[i].id == postId) {
+         let commentobj = {
+           userName: this.UsersService.user.userName,
+           comment: this.commentText[postindex],
+         };
+         this.posts[i].comments.push(commentobj);
+         this.commentText[postindex] = '';
+         this.ps.updatecomments(this.posts[i]).then(res => { console.log(res) }).catch(err => {
+           console.log(err);
+
+         });
+       }
+
+    }
+
+  }
+
   postschema = {
     userName: '',
     imageURL: '',
     text: '',
     likes: [],
-    comment: [{ userName: '', comment: '' }],
+    comments: [
+      {
+        userName: '',
+        comment: '',
+      },
+    ],
   };
 }
